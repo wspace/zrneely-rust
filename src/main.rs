479f1e07df863dfff4765aa306ec7176437af02e
Zachary Neely <zrneely@mit.edu>
2016-02-15 13:09:21 -0500

    Add organization levels to run tests

--- src/main.rs
+++ src/main.rs
@@ -61,56 +61,63 @@ fn main() {
 
 #[cfg(test)]
 mod runtest {
-    use std::collections::HashMap;
-
-    use parsers::program;
-    use wsstd::{Context, Label, Number};
-    use super::{parse, get_native_function};
-
     macro_rules! gen_tests {
-        ( $($name:ident: $program:expr => ($stack: expr, $heap: expr);)* ) => {
-            $(
-                #[test]
-                fn $name() {
-                    let program = parse($program).expect("Parsing failed!");
-                    let mut context = Context::new();
-                    {
-                        let program = get_native_function(program, &mut context);
-                        program.execute();
+        ( $($pkg:ident: {
+            $($name:ident: $program:expr => ($stack: expr, $heap: expr);)*
+        })*  ) => {
+
+            $(mod $pkg {
+                use parsers::program;
+                use wsstd::Context;
+                use ::{parse, get_native_function};
+
+                $(
+                    #[test]
+                    fn $name() {
+                        let program = parse($program).expect("Parsing failed!");
+                        let mut context = Context::new();
+                        {
+                            let program = get_native_function(program, &mut context);
+                            program.execute();
+                        }
+                        if let Some(stack) = $stack {
+                            // we reverse the stack here so that the top of the stack
+                            // is at the left in test cases, which is easier to read
+                            context.stack.reverse();
+                            assert_eq!(context.stack, &stack);
+                        }
+                        if let Some(heap) = $heap {
+                            assert_eq!(context.heap, heap);
+                        }
                     }
-                    if let Some(stack) = $stack {
-                        // we reverse the stack here so that the top of the stack
-                        // is at the left in test cases, which is easier to read
-                        context.stack.reverse();
-                        assert_eq!(context.stack, stack);
-                    }
-                    if let Some(heap) = $heap {
-                        assert_eq!(context.heap, heap);
-                    }
-                }
-            )*
-        }
+                )*
+            })*
+        };
     }
 
     gen_tests! {
-        // push 1
-        push:           b"    \t\n"                       => (Some(&[1]),         None);
-        // push 1, duplicate
-        duplicate:      b"    \t\n \n "                   => (Some(&[1, 1]),      None);
-        // push 1, pop
-        pop:            b"    \t\n \n\n"                  => (Some(&[]),          None);
-        // push 1, push 0, swap
-        swap:           b"    \t\n    \n \n\t"            => (Some(&[1, 0]),      None);
-        // push 0, push 1, copy 1
-        copy:           b"   \n    \t\n \t   \t\n"        => (Some(&[0, 1, 0]),   None);
+        stack: {
+            // push 1
+            push:       b"    \t\n"                       => (Some([1]),         None);
+            // push 1, duplicate
+            duplicate:  b"    \t\n \n "                   => (Some([1, 1]),      None);
+            // push 1, pop
+            pop:        b"    \t\n \n\n"                  => (Some([]),          None);
+            // push 1, push 0, swap
+            swap:       b"    \t\n    \n \n\t"            => (Some([1, 0]),      None);
+            // push 0, push 1, copy 1
+            copy:       b"   \n    \t\n \t   \t\n"        => (Some([0, 1, 0]),   None);
+        }
 
-        // push 1, push 3, add
-        add:            b"   \t\n   \t\t\n\t   "          => (Some(&[4]),         None);
-        // push -1, push 3, add
-        add_neg:        b"  \t\t\n   \t\t\n\t   "         => (Some(&[2]),         None);
-        // push 3, push 1, subtract
-        sub:            b"   \t\t\n   \t\n\t  \t"         => (Some(&[2]),         None);
-        // push 1, push 3, subtract
-        sub_neg:        b"   \t\n   \t\t\n\t  \t"         => (Some(&[-2]),        None);
+        arithmetic: {
+            // push 1, push 3, add
+            add:        b"   \t\n   \t\t\n\t   "          => (Some([4]),         None);
+            // push -1, push 3, add
+            add_neg:    b"  \t\t\n   \t\t\n\t   "         => (Some([2]),         None);
+            // push 3, push 1, subtract
+            sub:        b"   \t\t\n   \t\n\t  \t"         => (Some([2]),         None);
+            // push 1, push 3, subtract
+            sub_neg:    b"   \t\n   \t\t\n\t  \t"         => (Some([-2]),        None);
+        }
     }
 }
